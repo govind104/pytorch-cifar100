6dfba08629bd718b21c11eac36a04aa2cef28bd2
govind104 <137510180+govind104@users.noreply.github.com>
2024-11-17 14:56:01 +0000

    Update experiment_builder.py

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -152,7 +152,7 @@ class ExperimentBuilder(nn.Module):
         """
         ########################################
         for name, value in named_parameters:
-            if 'weight' in name and ('conv_bn' in name and '_bn_' not in name):
+            if 'weight' in name and ('conv_' in name and ('_bn_' not in name or 'input_' in name)):
                 all_grads.append(value.grad.abs().mean().item())
                 if 'layer_dict' in name:
                     layers.append(name[11:].replace('.layer_dict.', '_').replace('.weight',''))
