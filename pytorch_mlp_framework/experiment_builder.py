0928c03d392118ace8b3e1d9f4045937497f157b
govind104 <137510180+govind104@users.noreply.github.com>
2024-11-17 00:54:05 +0000

    Update experiment_builder.py

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -153,7 +153,7 @@ class ExperimentBuilder(nn.Module):
             if 'weight' in name:
                 all_grads.append(value.grad.abs().mean().item())
                 if 'layer_dict' in name:
-                    layers.append(name[11:].replace('.layer_dict.', '_').replace('.', '').replace('.weight',''))
+                    layers.append(name[11:].replace('.layer_dict.', '_').replace('.weight',''))
                 else:
                     layers.append('weight_' + name.replace('.weight',''))
         ########################################
